84afaded7fd51d7dd0847efeff0f41bb8d54949d
1abinot <labi.aziri1@gmail.com>
2022-06-03 18:13:43 +0200

    Every Statement

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,36 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class SILab2Test {
+
+    private List<String> createList(String... elems) {
+        return new ArrayList<>(Arrays.asList(elems));
+    }
+
+    @Test
+    void everyStatementTest() {
+        IllegalArgumentException ex;
+        List<String> expectedList;
+        ex = assertThrows(IllegalArgumentException.class, () -> SILab2.function(createList()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        ex = assertThrows(IllegalArgumentException.class, () -> SILab2.function(createList("0", "0", "#")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+
+
+        expectedList = Arrays.asList("#", "#", "#", "#");
+        assertEquals(expectedList, SILab2.function(createList("#", "#", "#", "#")));
+
+        expectedList = Arrays.asList("#", "#", "2", "#", "#", "#", "2", "#", "3", "#", "2", "#", "#", "#", "2", "#");
+        assertEquals(expectedList, SILab2.function(createList("#", "#","0","#", "#", "#", "0", "#", "0", "#", "0", "#", "#", "#", "0", "#")));
+
+    }
+
+   
+}
