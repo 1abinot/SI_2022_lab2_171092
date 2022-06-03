223d13199cf10dbe8be5af5bf382c0c873265973
1abinot <labi.aziri1@gmail.com>
2022-06-03 18:14:48 +0200

    Every Branch

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -29,8 +29,31 @@ public class SILab2Test {
 
         expectedList = Arrays.asList("#", "#", "2", "#", "#", "#", "2", "#", "3", "#", "2", "#", "#", "#", "2", "#");
         assertEquals(expectedList, SILab2.function(createList("#", "#","0","#", "#", "#", "0", "#", "0", "#", "0", "#", "#", "#", "0", "#")));
+    }
+
+
+    @Test
+    void everyBranchTest() {
+        IllegalArgumentException ex;
+        List<String> expectedList;
+        ex = assertThrows(IllegalArgumentException.class, () -> SILab2.function(createList()));
+        assertTrue(ex.getMessage().contains("List length should be greater than 0"));
+
+        ex = assertThrows(IllegalArgumentException.class, () -> SILab2.function(createList("0", "0", "#")));
+        assertTrue(ex.getMessage().contains("List length should be a perfect square"));
+
+
+        expectedList = Arrays.asList("#", "#", "#", "#");
+        assertEquals(expectedList, SILab2.function(createList("#", "#", "#", "#")));
+
+
+        expectedList = Arrays.asList("0", "0", "0", "0");
+        assertEquals(expectedList, SILab2.function(createList("0", "0", "0", "0")));
+
+
+        expectedList = Arrays.asList("#", "#", "2", "#", "#", "#", "2", "#", "3", "#", "2", "#", "#", "#", "2", "#");
+        assertEquals(expectedList, SILab2.function(createList("#", "#","0","#", "#", "#", "0", "#", "0", "#", "0", "#", "#", "#", "0", "#")));
 
     }
 
-   
 }
